34fdf82908458728484370bc2a13686e822f1f4f
omprakash kumar <omprakashkumarglb@gmail.com>
2025-01-10 15:13:29 +0530

    re construct login page

--- BACKENDS/index.js
+++ BACKENDS/index.js
@@ -8,37 +8,3 @@ const rout = 8000;
 app.listen(rout, () => {
    console.log(`Server is running on port ${rout}`);
 });
-
-
-app.get("/" , (req , res) => {
-   res.render("home.ejs");
-});
-
-
-app.get("/login" , (req , res) => {
-   res.render("login.ejs");
-});
-
-
-app.get("/rejester" , (req , res) => {
-   res.render("rejester.ejs");
-});
-
-
-
-// Create the connection to database
-const connection = await mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    database: 'omprakash',
-    password : 'Noida@18',
-  });
-
-  try{
-     connection.query("SHOW TABLES" ,(err , result)=>{
-        if (err) throw err
-        console.log(result)
-     })
-  }catch(err){
-    console.log(err);
-  }
