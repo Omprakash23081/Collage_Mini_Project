3777909ae8ab2a8dd6b54172d07ea47a792f4d86
omprakash kumar <omprakashkumarglb@gmail.com>
2026-01-11 13:02:17 +0530

    Delete BACKENDS directory

--- BACKENDS/constants/URL.js
+++
@@ -1,2 +0,0 @@
-//the database name used in the application
-export const DB_NAME = "omprakash";
--- BACKENDS/constent.js
+++
@@ -1,2 +0,0 @@
-export const API_URL = "https://api.hellowduniya.netlify.app";
-export const DB_NAME = "omprakash";
