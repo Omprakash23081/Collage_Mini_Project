e7d87b380fb507650865ce200d177fe934d8d627
omprakash kumar <omprakashkumarglb@gmail.com>
2026-01-11 12:21:51 +0530

    add mackend url and change layout of frontend

--- FRONTEND/src/utils/axios.js
+++ FRONTEND/src/utils/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 //it will define base url so that we can just referance verable and mathod by witch we will call backend
 const apiClient = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000/api",
+  baseURL: import.meta.env.VITE_API_URL / api || "http://localhost:3000/api",
   withCredentials: true,
 });
 
