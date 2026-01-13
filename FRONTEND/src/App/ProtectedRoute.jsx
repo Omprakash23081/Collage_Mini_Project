8abbdc35a7d7c274b5462b9e92aec9977e4f8fee
omprakash kumar <omprakashkumarglb@gmail.com>
2026-01-13 20:41:27 +0530

    make responsive for mobile

--- FRONTEND/src/App/ProtectedRoute.jsx
+++ FRONTEND/src/App/ProtectedRoute.jsx
@@ -5,7 +5,9 @@ import { AuthContext } from "../context/AuthContext";
 const ProtectedRoute = ({ children }) => {
   const { isAuthenticated, loading } = useContext(AuthContext);
 
-  if (loading) return null;
+  if (loading) {
+    return <div style={{ minHeight: "100vh" }} />;
+  }
 
   if (!isAuthenticated) {
     return <Navigate to="/login" replace />;
