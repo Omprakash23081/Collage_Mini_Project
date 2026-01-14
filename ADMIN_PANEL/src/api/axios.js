0f4b0c0043d16a646a46e03ca3afcb09616665bc
omprakash kumar <omprakashkumarglb@gmail.com>
2026-01-15 00:02:35 +0530

    add backend url

--- ADMIN_PANEL/src/api/axios.js
+++ ADMIN_PANEL/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://collage-mini-project-090y.onrender.com/api",
   withCredentials: true, // Important for cookies
   headers: {
     "Content-Type": "application/json",
